d0bbe825fff47f10d789be2df0a508d38cc40090
SeeStarz <seestarz@tutamail.com>
2024-10-25 19:39:54 +0700

    Added triangle

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use glfw::{Action, Context, Key};
+use std::ptr;
 
 fn main() {
     // Initialize GLFW
@@ -28,9 +29,101 @@ fn main() {
     window.set_key_polling(true);
     window.make_current();
 
-    unsafe {
+    let vertices: [f32; 9] = [0.0, 0.5, 0.0, -0.5, -0.5, 0.0, 0.5, -0.5, 0.0];
+    let vs_source: &str = "#version 330 core
+layout (location = 0) in vec3 pos;
+
+void main()
+{
+    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
+}\0";
+
+    let frag_source = "#version 330 core
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+} \0";
+
+    let (shader_program, vao) = unsafe {
         gl::Viewport(0, 0, 800, 600);
-    }
+        let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
+        assert_ne!(vertex_shader, 0);
+        gl::ShaderSource(
+            vertex_shader,
+            1,
+            &(vs_source.as_bytes().as_ptr() as *const i8),
+            ptr::null(),
+        );
+        gl::CompileShader(vertex_shader);
+
+        let mut success = 0;
+        gl::GetShaderiv(vertex_shader, gl::COMPILE_STATUS, &mut success);
+
+        let mut info_log = 0;
+        gl::GetShaderInfoLog(vertex_shader, 512, &mut 512, &mut info_log);
+        if success != 0 {
+            println!("{success} {info_log:?}")
+        }
+
+        let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+        assert_ne!(fragment_shader, 0);
+        gl::ShaderSource(
+            fragment_shader,
+            1,
+            &(frag_source.as_bytes().as_ptr() as *const i8),
+            ptr::null(),
+        );
+        gl::CompileShader(fragment_shader);
+
+        let shader_program = gl::CreateProgram();
+        gl::AttachShader(shader_program, vertex_shader);
+        gl::AttachShader(shader_program, fragment_shader);
+        gl::LinkProgram(shader_program);
+
+        gl::GetProgramiv(shader_program, gl::LINK_STATUS, &mut success);
+        if success != 0 {
+            gl::GetProgramInfoLog(shader_program, 512, &mut 512, &mut info_log);
+            println!("{success} {info_log:?}")
+        }
+
+        gl::UseProgram(shader_program);
+        gl::DeleteShader(vertex_shader);
+        gl::DeleteShader(fragment_shader);
+
+        let mut vao = 0;
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+
+        let mut vbo = 0;
+        gl::GenBuffers(1, &mut vbo);
+        assert_ne!(vbo, 0);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            size_of_val(&vertices) as isize,
+            vertices.as_ptr() as *const _,
+            gl::STATIC_DRAW,
+        );
+        println!("{}", size_of_val(&vertices));
+        println!("{}", size_of_val(&vertices[0]));
+
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            3 * size_of::<f32>() as i32,
+            ptr::null(),
+        );
+        println!("{}", 3 * size_of::<f32>() as i32);
+
+        gl::EnableVertexAttribArray(0);
+
+        (shader_program, vao)
+    };
 
     // Main loop
     while !window.should_close() {
@@ -42,6 +135,10 @@ fn main() {
         unsafe {
             gl::ClearColor(0.2, 0.0, 0.2, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            gl::UseProgram(shader_program);
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
         }
 
         window.swap_buffers();
