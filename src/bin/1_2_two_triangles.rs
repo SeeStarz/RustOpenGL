6fc01eb731bcbcb86d07c56823f9076199cce2ff
SeeStarz <seestarz@tutamail.com>
2024-10-28 08:55:21 +0700

    Add exercises for triangles

--- src/bin/1_2_two_triangles.rs
+++ src/bin/1_2_two_triangles.rs
@@ -11,20 +11,12 @@ void main()
     gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
 }\0";
 
-const FRAGMENT_SHADER_SOURCE_RED: &str = "#version 330 core
+const FRAGMENT_SHADER_SOURCE: &str = "#version 330 core
 out vec4 FragColor;
 
 void main()
 {
     FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
-}\0";
-
-const FRAGMENT_SHADER_SOURCE_BLUE: &str = "#version 330 core
-out vec4 FragColor;
-
-void main()
-{
-    FragColor = vec4(0.0f, 0.0f, 1.0f, 1.0f);
 }\0";
 
 fn main() {
@@ -72,7 +64,7 @@ fn main() {
     ];
 
     // Obtain the shader program and vertex array object
-    let (shader_program_red, shader_program_blue, vao1, vao2) = unsafe {
+    let (shader_program, vao) = unsafe {
         // Create vertex shader
         let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
         assert_ne!(vertex_shader, 0);
@@ -102,25 +94,25 @@ fn main() {
             );
         }
 
-        // Create red fragment shader
-        let fragment_shader_red = gl::CreateShader(gl::FRAGMENT_SHADER);
-        assert_ne!(fragment_shader_red, 0);
+        // Create fragment shader
+        let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+        assert_ne!(fragment_shader, 0);
         gl::ShaderSource(
-            fragment_shader_red,
+            fragment_shader,
             1,
-            &(FRAGMENT_SHADER_SOURCE_RED.as_bytes().as_ptr() as *const i8),
+            &(FRAGMENT_SHADER_SOURCE.as_bytes().as_ptr() as *const i8),
             ptr::null(),
         );
-        gl::CompileShader(fragment_shader_red);
+        gl::CompileShader(fragment_shader);
 
         // Check status of fragment shader
         let mut success = gl::FALSE as GLint;
-        gl::GetShaderiv(fragment_shader_red, gl::COMPILE_STATUS, &mut success);
+        gl::GetShaderiv(fragment_shader, gl::COMPILE_STATUS, &mut success);
         if success != gl::TRUE as GLint {
             let mut info_log: [u8; 256] = [0; 256];
             let mut length = 0;
             gl::GetShaderInfoLog(
-                fragment_shader_red,
+                fragment_shader,
                 512,
                 &mut length,
                 info_log.as_mut_ptr() as *mut GLchar,
@@ -131,54 +123,20 @@ fn main() {
             );
         }
 
-        // Create blue fragment shader
-        let fragment_shader_blue = gl::CreateShader(gl::FRAGMENT_SHADER);
-        assert_ne!(fragment_shader_blue, 0);
-        gl::ShaderSource(
-            fragment_shader_blue,
-            1,
-            &(FRAGMENT_SHADER_SOURCE_BLUE.as_bytes().as_ptr() as *const i8),
-            ptr::null(),
-        );
-        gl::CompileShader(fragment_shader_blue);
-
-        // Check status of fragment shader
-        let mut success = gl::FALSE as GLint;
-        gl::GetShaderiv(fragment_shader_blue, gl::COMPILE_STATUS, &mut success);
-        if success != gl::TRUE as GLint {
-            let mut info_log: [u8; 256] = [0; 256];
-            let mut length = 0;
-            gl::GetShaderInfoLog(
-                fragment_shader_blue,
-                512,
-                &mut length,
-                info_log.as_mut_ptr() as *mut GLchar,
-            );
-            println!(
-                "FRAGMENT_SHADER ERROR: {}",
-                str::from_utf8(&info_log[..length as usize]).expect("Cannot get error message")
-            );
-        }
-
-        // Create red shader program
-        let shader_program_red = gl::CreateProgram();
-        gl::AttachShader(shader_program_red, vertex_shader);
-        gl::AttachShader(shader_program_red, fragment_shader_red);
-        gl::LinkProgram(shader_program_red);
-
-        let shader_program_blue = gl::CreateProgram();
-        gl::AttachShader(shader_program_blue, vertex_shader);
-        gl::AttachShader(shader_program_blue, fragment_shader_blue);
-        gl::LinkProgram(shader_program_blue);
+        // Create shader program
+        let shader_program = gl::CreateProgram();
+        gl::AttachShader(shader_program, vertex_shader);
+        gl::AttachShader(shader_program, fragment_shader);
+        gl::LinkProgram(shader_program);
 
         // Check shader program status
         let mut success = gl::FALSE as GLint;
-        gl::GetProgramiv(shader_program_red, gl::LINK_STATUS, &mut success);
+        gl::GetProgramiv(shader_program, gl::LINK_STATUS, &mut success);
         if success != gl::TRUE as GLint {
             let mut info_log: [u8; 256] = [0; 256];
             let mut length = 0;
             gl::GetProgramInfoLog(
-                shader_program_red,
+                shader_program,
                 512,
                 &mut length,
                 info_log.as_mut_ptr() as *mut GLchar,
@@ -191,21 +149,20 @@ fn main() {
 
         // Delete unused shader
         gl::DeleteShader(vertex_shader);
-        gl::DeleteShader(fragment_shader_red);
-        gl::DeleteShader(fragment_shader_blue);
+        gl::DeleteShader(fragment_shader);
 
         // Create vertex array object
-        let mut vao1 = 0;
-        gl::GenVertexArrays(1, &mut vao1);
-        gl::BindVertexArray(vao1);
+        let mut vao = 0;
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
 
         // Create vertex buffer object
-        let mut vbo1 = 0;
-        gl::GenBuffers(1, &mut vbo1);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo1);
+        let mut vbo = 0;
+        gl::GenBuffers(1, &mut vbo);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
         gl::BufferData(
             gl::ARRAY_BUFFER,
-            (size_of_val(&vertices) / 2) as isize,
+            size_of_val(&vertices) as isize,
             vertices.as_ptr() as *const _,
             gl::STATIC_DRAW,
         );
@@ -224,38 +181,8 @@ fn main() {
         // Unbind vertex array object
         gl::BindVertexArray(0);
 
-        // Create another vertex array object
-        let mut vao2 = 0;
-        gl::GenVertexArrays(1, &mut vao2);
-        gl::BindVertexArray(vao2);
-
-        // Create another vertex buffer object
-        let mut vbo2 = 0;
-        gl::GenBuffers(1, &mut vbo2);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo2);
-        gl::BufferData(
-            gl::ARRAY_BUFFER,
-            (size_of_val(&vertices) / 2) as isize,
-            vertices.as_ptr().offset(9) as *const _,
-            gl::STATIC_DRAW,
-        );
-
-        // Create another vertex attribute pointer
-        gl::VertexAttribPointer(
-            0,
-            3,
-            gl::FLOAT,
-            gl::FALSE,
-            3 * size_of_val(&vertices[0]) as i32,
-            ptr::null(),
-        );
-        gl::EnableVertexAttribArray(0);
-
-        // Unbind vertex array object
-        gl::BindVertexArray(0);
-
         // Return shader program and vertex array object
-        (shader_program_red, shader_program_blue, vao1, vao2)
+        (shader_program, vao)
     };
 
     // Main loop
@@ -271,14 +198,10 @@ fn main() {
             gl::ClearColor(1., 1., 1., 1.);
             gl::Clear(gl::COLOR_BUFFER_BIT);
 
-            gl::UseProgram(shader_program_blue);
-            gl::BindVertexArray(vao1);
+            gl::UseProgram(shader_program);
+            gl::BindVertexArray(vao);
             gl::DrawArrays(gl::TRIANGLES, 0, 3);
-
-            gl::UseProgram(shader_program_red);
-            gl::BindVertexArray(vao2);
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
-
+            gl::DrawArrays(gl::TRIANGLES, 3, 3);
             gl::BindVertexArray(0);
         }
 
--- src/bin/1_3_two_vao_tbo.rs
+++ src/bin/1_3_two_vao_tbo.rs
@@ -11,20 +11,12 @@ void main()
     gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
 }\0";
 
-const FRAGMENT_SHADER_SOURCE_RED: &str = "#version 330 core
+const FRAGMENT_SHADER_SOURCE: &str = "#version 330 core
 out vec4 FragColor;
 
 void main()
 {
     FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
-}\0";
-
-const FRAGMENT_SHADER_SOURCE_BLUE: &str = "#version 330 core
-out vec4 FragColor;
-
-void main()
-{
-    FragColor = vec4(0.0f, 0.0f, 1.0f, 1.0f);
 }\0";
 
 fn main() {
@@ -72,7 +64,7 @@ fn main() {
     ];
 
     // Obtain the shader program and vertex array object
-    let (shader_program_red, shader_program_blue, vao1, vao2) = unsafe {
+    let (shader_program, vao1, vao2) = unsafe {
         // Create vertex shader
         let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
         assert_ne!(vertex_shader, 0);
@@ -102,25 +94,25 @@ fn main() {
             );
         }
 
-        // Create red fragment shader
-        let fragment_shader_red = gl::CreateShader(gl::FRAGMENT_SHADER);
-        assert_ne!(fragment_shader_red, 0);
+        // Create fragment shader
+        let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+        assert_ne!(fragment_shader, 0);
         gl::ShaderSource(
-            fragment_shader_red,
+            fragment_shader,
             1,
-            &(FRAGMENT_SHADER_SOURCE_RED.as_bytes().as_ptr() as *const i8),
+            &(FRAGMENT_SHADER_SOURCE.as_bytes().as_ptr() as *const i8),
             ptr::null(),
         );
-        gl::CompileShader(fragment_shader_red);
+        gl::CompileShader(fragment_shader);
 
         // Check status of fragment shader
         let mut success = gl::FALSE as GLint;
-        gl::GetShaderiv(fragment_shader_red, gl::COMPILE_STATUS, &mut success);
+        gl::GetShaderiv(fragment_shader, gl::COMPILE_STATUS, &mut success);
         if success != gl::TRUE as GLint {
             let mut info_log: [u8; 256] = [0; 256];
             let mut length = 0;
             gl::GetShaderInfoLog(
-                fragment_shader_red,
+                fragment_shader,
                 512,
                 &mut length,
                 info_log.as_mut_ptr() as *mut GLchar,
@@ -131,54 +123,20 @@ fn main() {
             );
         }
 
-        // Create blue fragment shader
-        let fragment_shader_blue = gl::CreateShader(gl::FRAGMENT_SHADER);
-        assert_ne!(fragment_shader_blue, 0);
-        gl::ShaderSource(
-            fragment_shader_blue,
-            1,
-            &(FRAGMENT_SHADER_SOURCE_BLUE.as_bytes().as_ptr() as *const i8),
-            ptr::null(),
-        );
-        gl::CompileShader(fragment_shader_blue);
-
-        // Check status of fragment shader
-        let mut success = gl::FALSE as GLint;
-        gl::GetShaderiv(fragment_shader_blue, gl::COMPILE_STATUS, &mut success);
-        if success != gl::TRUE as GLint {
-            let mut info_log: [u8; 256] = [0; 256];
-            let mut length = 0;
-            gl::GetShaderInfoLog(
-                fragment_shader_blue,
-                512,
-                &mut length,
-                info_log.as_mut_ptr() as *mut GLchar,
-            );
-            println!(
-                "FRAGMENT_SHADER ERROR: {}",
-                str::from_utf8(&info_log[..length as usize]).expect("Cannot get error message")
-            );
-        }
-
-        // Create red shader program
-        let shader_program_red = gl::CreateProgram();
-        gl::AttachShader(shader_program_red, vertex_shader);
-        gl::AttachShader(shader_program_red, fragment_shader_red);
-        gl::LinkProgram(shader_program_red);
-
-        let shader_program_blue = gl::CreateProgram();
-        gl::AttachShader(shader_program_blue, vertex_shader);
-        gl::AttachShader(shader_program_blue, fragment_shader_blue);
-        gl::LinkProgram(shader_program_blue);
+        // Create shader program
+        let shader_program = gl::CreateProgram();
+        gl::AttachShader(shader_program, vertex_shader);
+        gl::AttachShader(shader_program, fragment_shader);
+        gl::LinkProgram(shader_program);
 
         // Check shader program status
         let mut success = gl::FALSE as GLint;
-        gl::GetProgramiv(shader_program_red, gl::LINK_STATUS, &mut success);
+        gl::GetProgramiv(shader_program, gl::LINK_STATUS, &mut success);
         if success != gl::TRUE as GLint {
             let mut info_log: [u8; 256] = [0; 256];
             let mut length = 0;
             gl::GetProgramInfoLog(
-                shader_program_red,
+                shader_program,
                 512,
                 &mut length,
                 info_log.as_mut_ptr() as *mut GLchar,
@@ -191,8 +149,7 @@ fn main() {
 
         // Delete unused shader
         gl::DeleteShader(vertex_shader);
-        gl::DeleteShader(fragment_shader_red);
-        gl::DeleteShader(fragment_shader_blue);
+        gl::DeleteShader(fragment_shader);
 
         // Create vertex array object
         let mut vao1 = 0;
@@ -255,7 +212,7 @@ fn main() {
         gl::BindVertexArray(0);
 
         // Return shader program and vertex array object
-        (shader_program_red, shader_program_blue, vao1, vao2)
+        (shader_program, vao1, vao2)
     };
 
     // Main loop
@@ -271,14 +228,11 @@ fn main() {
             gl::ClearColor(1., 1., 1., 1.);
             gl::Clear(gl::COLOR_BUFFER_BIT);
 
-            gl::UseProgram(shader_program_blue);
+            gl::UseProgram(shader_program);
             gl::BindVertexArray(vao1);
             gl::DrawArrays(gl::TRIANGLES, 0, 3);
-
-            gl::UseProgram(shader_program_red);
             gl::BindVertexArray(vao2);
             gl::DrawArrays(gl::TRIANGLES, 0, 3);
-
             gl::BindVertexArray(0);
         }
 
